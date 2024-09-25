6a11d08350702cc6c6e2b660a7287abc12af3d22
Jenifer Irwin <jenifer.irwin@gmail.com>
2024-09-25 14:09:21 -0400

    Add project files.

---
+++ SpeedySteward/SubModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace SpeedySteward
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            Harmony harmony = new Harmony("speedy_steward");
+            harmony.PatchAll();
+        }
+
+        public override void OnMissionBehaviorInitialize(Mission mission)
+        {
+            base.OnMissionBehaviorInitialize(mission);
+        }
+
+        protected override void InitializeGameStarter(Game game, IGameStarter starterObject)
+        {
+            base.InitializeGameStarter(game, starterObject);
+        }
+    }
+
+}
---
+++ SpeedySteward/TransferAllPatch.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Inventory;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Inventory;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+
+namespace SpeedySteward
+{
+    [HarmonyPatch(typeof(SPInventoryVM), "TransferAll")]
+    public class TransferAllPatch
+    {
+        public static bool Prefix(SPInventoryVM __instance, bool isBuy, InventoryLogic ____inventoryLogic, CharacterObject ____currentCharacter)
+        {
+            __instance.IsRefreshed = false;
+            List<TransferCommand> list = new List<TransferCommand>(__instance.LeftItemListVM.Count);
+            MBBindingList<SPItemVM> mBBindingList = (isBuy ? __instance.LeftItemListVM : __instance.RightItemListVM);
+            MobileParty party = (isBuy ? MobileParty.MainParty : ____inventoryLogic.OppositePartyFromListener?.MobileParty);
+            float num = 0f;
+            var getCapacityBudget = AccessTools.Method(typeof(SPInventoryVM), "GetCapacityBudget");
+            float capacityBudget = (float) getCapacityBudget.Invoke(__instance, new object[] { party, isBuy });
+            float num2 = mBBindingList.FirstOrDefault((SPItemVM x) => !x.IsFiltered && !x.IsLocked)?.ItemRosterElement.EquipmentElement.GetEquipmentElementWeight() ?? 0f;
+            bool flag = capacityBudget <= num2;
+            InventoryLogic.InventorySide fromSide = ((!isBuy) ? InventoryLogic.InventorySide.PlayerInventory : InventoryLogic.InventorySide.OtherInventory);
+            InventoryLogic.InventorySide inventorySide = (isBuy ? InventoryLogic.InventorySide.PlayerInventory : InventoryLogic.InventorySide.OtherInventory);
+            List<SPItemVM> list2 = new List<SPItemVM>();
+            bool flag2 = ____inventoryLogic.CanInventoryCapacityIncrease(inventorySide);
+            for (int i = 0; i < mBBindingList.Count; i++)
+            {
+                SPItemVM sPItemVM = mBBindingList[i];
+                if (sPItemVM == null || sPItemVM.IsFiltered || sPItemVM == null || sPItemVM.IsLocked || sPItemVM == null || !sPItemVM.IsTransferable)
+                {
+                    InformationManager.DisplayMessage(new InformationMessage("Item skipped..."));
+                    continue;
+                }
+                int num3 = sPItemVM.ItemRosterElement.Amount;
+                if (!flag)
+                {
+                    float equipmentElementWeight = sPItemVM.ItemRosterElement.EquipmentElement.GetEquipmentElementWeight();
+                    float num4 = num + equipmentElementWeight * (float)num3;
+                    if (flag2)
+                    {
+                        if (____inventoryLogic.GetCanItemIncreaseInventoryCapacity(mBBindingList[i].ItemRosterElement.EquipmentElement.Item))
+                        {
+                            list2.Add(mBBindingList[i]);
+                            continue;
+                        }
+                        if (num4 >= capacityBudget && list2.Count > 0)
+                        {
+                            List<TransferCommand> list3 = new List<TransferCommand>(list2.Count);
+                            for (int j = 0; j < list2.Count; j++)
+                            {
+                                SPItemVM sPItemVM2 = list2[j];
+                                TransferCommand item = TransferCommand.Transfer(sPItemVM2.ItemRosterElement.Amount, fromSide, inventorySide, sPItemVM2.ItemRosterElement, EquipmentIndex.None, EquipmentIndex.None, ____currentCharacter, !__instance.IsInWarSet);
+                                list3.Add(item);
+                            }
+                            ____inventoryLogic.AddTransferCommands(list3);
+                            list3.Clear();
+                            list2.Clear();
+                            capacityBudget = (float) getCapacityBudget.Invoke(__instance, new object[] { party, isBuy });
+                        }
+                    }
+                    if (num3 > 0 && num4 > capacityBudget)
+                    {
+                        num3 = MBMath.ClampInt(num3, 0, TaleWorlds.Library.MathF.Floor((capacityBudget - num) / equipmentElementWeight));
+                        i = mBBindingList.Count;
+                    }
+                    num += (float)num3 * equipmentElementWeight;
+                }
+                if (num3 > 0)
+                {
+                    TransferCommand item2 = TransferCommand.Transfer(num3, fromSide, inventorySide, sPItemVM.ItemRosterElement, EquipmentIndex.None, EquipmentIndex.None, ____currentCharacter, !__instance.IsInWarSet);
+                    list.Add(item2);
+                }
+            }
+            if (num <= capacityBudget)
+            {
+                foreach (SPItemVM item4 in list2)
+                {
+                    TransferCommand item3 = TransferCommand.Transfer(item4.ItemRosterElement.Amount, fromSide, inventorySide, item4.ItemRosterElement, EquipmentIndex.None, EquipmentIndex.None, ____currentCharacter, !__instance.IsInWarSet);
+                    list.Add(item3);
+                }
+            }
+            ____inventoryLogic.AddTransferCommands(list);
+            var refreshInformationValues = AccessTools.Method(typeof(SPInventoryVM), "RefreshInformationValues");
+            var executeRemoveZeroCounts = AccessTools.Method(typeof(SPInventoryVM), "ExecuteRemoveZeroCounts");
+            refreshInformationValues.Invoke(__instance, null);
+            executeRemoveZeroCounts.Invoke(__instance, null);
+            __instance.IsRefreshed = true;
+            return false;
+        }
+    }
+}
